e686d458c98d0ed85dd27ed3fe9f8633e29ae5a9
NHG <nhg@posteo.de>
2017-04-22 15:44:44 +0200

    what the randomcloud

---
+++ src/worldobjects/RandomCloud.cpp
@@ -0,0 +1,17 @@
+#include <random>
+using namespace std;
+
+#include "Cloud.h"
+#include "gameplay.h"
+using namespace gameplay;
+
+Cloud::Cloud(World *world):
+    WorldObjectSingle(world)
+{
+    std::default_random_engine generator;
+    std::uniform_real_distribution<float> distribution(minSpeed, maxSpeed);
+    speed = distribution(generator);
+
+    batch = new CPU::SpriteBatch("@cloud");
+    batch->scale.set(16, 16);
+}
